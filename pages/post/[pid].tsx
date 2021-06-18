03ae4f07602d8d368531b911e4a7caf13b7d54a4
Teddy-Seop <teddy.kim@onda.me>
2021-06-18 11:49:16 +0900

    Add static generation data

--- pages/post/[pid].tsx
+++ pages/post/[pid].tsx
@@ -1,15 +1,44 @@
+import { NextPage } from "next";
 import { useRouter } from "next/router";
 
-const Post = () => {
-  const router = useRouter();
-  const { pid, foo } = router.query;
+interface IPostProps {
+  posts: IPost[];
+}
 
-  return (
-    <div>
-      <div>Post: {pid}</div>
-      <div>foo: {foo}</div>
-    </div>
+interface IPost {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+const Post: NextPage<IPostProps> = ({ posts }) => {
+  const router = useRouter();
+  const { pid } = router.query;
+  const postItems = constructPostItems(posts);
+  return <div>{postItems}</div>;
+};
+
+Post.getInitialProps = async () => {
+  const posts = await fetch("https://jsonplaceholder.typicode.com/posts").then(
+    (response) => response.json()
   );
+
+  return {
+    posts,
+  };
+};
+
+const constructPostItems = (posts: IPost[]) => {
+  return posts.map((post) => {
+    return (
+      <div key={post.id}>
+        <div>Post: {post.id}</div>
+        <div>title: {post.title}</div>
+        <div>body: {post.body}</div>
+      </div>
+    );
+  });
 };
 
 export default Post;
